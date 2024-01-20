c4201136553b700adfbf0e1250a597114f12a2d6
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-20 03:03:59 -0800

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # install flask
 
 package{'flask':
-ensure   => 'installed',
-provider => 'pip3',
-version  => '2.1.0',
+  ensure   => 'installed',
+  provider => 'pip3',
+  version  => '2.1.0',
 }
