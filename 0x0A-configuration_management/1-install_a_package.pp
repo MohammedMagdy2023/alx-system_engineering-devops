463dc769a45e6e9e1b6b2639239fb0c9228a363a
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-31 07:05:51 -0800

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # Install flask V(2.1.0)
 
 class { 'python':
-  version    => '3',
-  pip        => 'present',
+  version => '3',
+  pip     => 'present',
 }
 
 python::pip { 'flask':
-  pkgname => 'Flask',
   ensure  => '2.1.0',
+  pkgname => 'Flask',
 }
