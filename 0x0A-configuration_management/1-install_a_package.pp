32c92ce2b716da4bf5b17cca5632e23d388163a9
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-31 07:22:25 -0800

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,7 @@
 
 exec { 'install python packages':
   command => 'pip3 install Flask==2.1.0',
-  path => ['/usr/bin/'],
-  unless => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+  path    => ['/usr/bin/'],
+  unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
 }
 
