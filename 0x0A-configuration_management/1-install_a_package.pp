9346563e18db81a124e96e8dc0a958185c529825
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-02 11:48:42 -0800

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # install flask from pip3 using  Puppet
 
-package { 'flask' :
-    ensure   => 'installed',
-    provider => 'pip3',
-    version  => '2.1.0',
-    }
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip'
+}
