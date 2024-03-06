2594bdcddc89a8f5b51855306f248054609d3084
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-03-06 15:33:55 -0800

    api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,14 +2,14 @@
 """
     This script uses reddit api
 """
-import requests
 
 
 def top_ten(subreddit):
     """The function to use to return top10 posts of the subreddit"""
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    import requests
 
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
     try:
         response = requests.get(url, headers=headers,
                                 allow_redirects=False)
